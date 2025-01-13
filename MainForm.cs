1f865102dd6ee71bd9708c36a5d453835fe23d0b
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-01-13 12:06:08 -0500

    refactor code and release system resources when speech stopped

--- MainForm.cs
+++ MainForm.cs
@@ -10,20 +10,26 @@ namespace WindowsTextReader
         List<VoiceInfo> voiceInfoList = new List<VoiceInfo>();
         public TextReader()
         {
-            // Configure the audio output.
-            Synth.SetOutputToDefaultAudioDevice();
             InitializeComponent();
         }
 
+        // **** REUSABLE METHODS *****
         private void ChangeVoice()
         {
+            if (Synth == null)
+            {
+                Synth = new SpeechSynthesizer();
+            }
             Synth.SelectVoice(voiceSelectionMenu.Text);
             genderLabel.Text = voiceInfoList.ElementAt(voiceSelectionMenu.SelectedIndex).Gender.ToString();
             langLabel.Text = voiceInfoList.ElementAt(voiceSelectionMenu.SelectedIndex).Culture.ToString();
         }
 
+        // **** UI METHODS *****
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Configure the audio output.
+            Synth.SetOutputToDefaultAudioDevice();
             foreach (var voice in Synth.GetInstalledVoices())
             {
                 var info = voice.VoiceInfo;
@@ -39,28 +45,38 @@ namespace WindowsTextReader
             readOutLoudTextBox.Text = "";
         }
 
-        private void readButton_Click(object sender, EventArgs e)
-        {
-            Synth.SpeakAsyncCancelAll();
-            // Speak the text in the textbox
-            var prompt = Synth.SpeakAsync(readOutLoudTextBox.Text);
-        }
-
         private void voiceSelectionMenu_SelectedIndexChanged(object sender, EventArgs e)
         {
             ChangeVoice();
         }
 
-        private void stopButton_Click(object sender, EventArgs e)
-        {
-            Synth.Pause();
-            Synth.SpeakAsyncCancelAll();
-            Synth.Resume();
-        }
-
         private void speedTrackBar_Scroll(object sender, EventArgs e)
         {
             Synth.Rate = speedTrackBar.Value * 2;
+        }
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            if (Synth != null)
+            {
+                Synth.SpeakAsyncCancelAll();
+                Synth.Dispose();
+                Synth = null;
+            }
+        }
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            if (Synth == null)
+            {
+                Synth = new SpeechSynthesizer();
+                Synth.SetOutputToDefaultAudioDevice();
+                ChangeVoice();
+            }
+            else
+            {
+                Synth.SpeakAsyncCancelAll();
+            }
+            // Speak the text in the textbox
+            Synth.SpeakAsync(readOutLoudTextBox.Text);
         }
     }
 }
