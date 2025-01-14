0961f15c5d1d6d4aadec252e9398f45c917dceba
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-01-14 05:38:41 -0500

    add function to check if is initialized before executing callback

--- MainForm.cs
+++ MainForm.cs
@@ -6,7 +6,7 @@ namespace WindowsTextReader
     public partial class TextReader : Form
     {
         // Initialize a new instance of the SpeechSynthesizer.
-        SpeechSynthesizer Synth = new SpeechSynthesizer();
+        SpeechSynthesizer? Synth = new SpeechSynthesizer();
         List<VoiceInfo> voiceInfoList = new List<VoiceInfo>();
         public TextReader()
         {
@@ -14,6 +14,20 @@ namespace WindowsTextReader
         }
 
         // **** REUSABLE METHODS *****
+        private void CheckSynth(Action? hasSynthCallback = null, Action? noSynthCallback = null)
+        {
+            if (Synth == null)
+            {
+                Synth = new SpeechSynthesizer();
+                Synth.SetOutputToDefaultAudioDevice();
+                ChangeVoice();
+                if (noSynthCallback != null) noSynthCallback();
+            }
+            else
+            {
+                if (hasSynthCallback != null) hasSynthCallback();
+            }
+        }
         private void ChangeVoice()
         {
             if (Synth == null)
@@ -29,7 +43,7 @@ namespace WindowsTextReader
         private void Form1_Load(object sender, EventArgs e)
         {
             // Configure the audio output.
-            Synth.SetOutputToDefaultAudioDevice();
+            Synth?.SetOutputToDefaultAudioDevice();
             foreach (var voice in Synth.GetInstalledVoices())
             {
                 var info = voice.VoiceInfo;
@@ -47,12 +61,12 @@ namespace WindowsTextReader
 
         private void voiceSelectionMenu_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ChangeVoice();
+            CheckSynth();
         }
 
         private void speedTrackBar_Scroll(object sender, EventArgs e)
         {
-            Synth.Rate = speedTrackBar.Value * 2;
+            CheckSynth(() => { Synth.Rate = speedTrackBar.Value * 2; });
         }
         private void stopButton_Click(object sender, EventArgs e)
         {
@@ -61,22 +75,16 @@ namespace WindowsTextReader
                 Synth.SpeakAsyncCancelAll();
                 Synth.Dispose();
                 Synth = null;
-            }
+            };
         }
         private void readButton_Click(object sender, EventArgs e)
         {
-            if (Synth == null)
-            {
-                Synth = new SpeechSynthesizer();
-                Synth.SetOutputToDefaultAudioDevice();
-                ChangeVoice();
-            }
-            else
+            CheckSynth(() =>
             {
                 Synth.SpeakAsyncCancelAll();
-            }
+            }, () => { });
             // Speak the text in the textbox
-            Synth.SpeakAsync(readOutLoudTextBox.Text);
+            Synth?.SpeakAsync(readOutLoudTextBox.Text);
         }
     }
 }
